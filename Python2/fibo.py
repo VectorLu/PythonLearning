4ff58d2614e41c610a56bc50c194cc9243c27f64
VectorLu <vectorpinglu@outlook.com>
2017-04-23 13:28:24 +0800

    Fibo main

--- Python2/fibo.py
+++ Python2/fibo.py
@@ -1,5 +1,9 @@
-fibs = [0, 1]
-num = input('How many Fibonacci numbers do you want? ')
-for i in range(num - 2):
-    fibs.append(fibs[-2] + fibs[-1])
-print fibs
+def fibs(num):
+    result = [0, 1]
+    for i in range(num-2):
+        result.append(result[-1] + result[-2])
+    return result
+
+if __name__ == '__main__':
+    num = input('How many Fibonacci numbers do you want? ')
+    fibs(num)
