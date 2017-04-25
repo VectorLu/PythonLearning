6b62d2091edf4972c9f6a5aff253f3f8c0317026
VectorLu <vectorpinglu@outlook.com>
2017-04-25 13:35:16 +0800

    An infinite sequence implementing the protocal

---
+++ Python2/sequenceProtocol.py
@@ -0,0 +1,38 @@
+def checkIndex(key):
+    """
+    Is the given key an acceptable index?
+
+    To be acceptable, the key should be a non-negative integer. If it is not an integer, a TypeError is raised; if it is negative, an IndexError is raised (since the sequence is of infinite lenth).
+    """
+    if not isinstance(key, (int, long)): raise TypeError
+    if key < 0: raise IndexError
+
+class ArithmeticSequence:
+    def __init__(self, start = 0, step = 1):
+        """
+        Initialize the arithmetic sequence.
+
+        start - the first value in the sequence
+        step - the difference between two adjacent values
+        changed - a dictionary of values that have been modified by the user
+        """
+        self.start = start
+        self.step = step
+        self.changed = {}
+
+    def __getitem__(self, key):
+        """
+        Get an item from the arithmetic sequence.
+        """
+        checkIndex(key)
+
+        try: return self.changed[key]    # Modifies?
+        except KeyError:
+            return self.start + key * self.step
+
+    def __setitem__(self, key, value):
+        """
+        Change an item in the arithmetic sequence.
+        """
+        checkIndex(key)
+        self.changed[key] = value
