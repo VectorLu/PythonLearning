161154dd3358ed4c44da84f12de0c6eede542d36
VectorLu <vectorpinglu@outlook.com>
2017-04-26 16:22:02 +0800

    Add a interesting one-line solution

--- Python2/eightQueens.py
+++ Python2/eightQueens.py
@@ -23,3 +23,15 @@ def prettyPrint(solution):
 if __name__ == '__main__':
     import random
     prettyPrint(random.choice(list(queens(8))))
+
+    for solution in queens(8):
+        print solution
+
+    '''
+    作者：est
+    链接：https://www.zhihu.com/question/37046157/answer/70747261
+    来源：知乎
+    著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+    '''
+    # 这是一个一行的解法
+    _=[__import__('sys').stdout.write("\n".join('.' * i + 'Q' + '.' * (8-i-1) for i in vec) + "\n===\n") for vec in __import__('itertools').permutations(xrange(8)) if 8 == len(set(vec[i]+i for i in xrange(8))) == len(set(vec[i]-i for i in xrange(8)))]
