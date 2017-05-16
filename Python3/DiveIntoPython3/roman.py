d30779dd51540f6a63e7520e4099510a087bee0a
VectorLu <vectorpinglu@outlook.com>
2017-05-16 21:38:25 +0800

    Add number type test

--- Python3/DiveIntoPython3/roman.py
+++ Python3/DiveIntoPython3/roman.py
@@ -15,11 +15,16 @@ roman_numeral_map = (('M',  1000),
 class OutOfRangeError(ValueError):
     pass
 
+class NotIntegerError(ValueError):
+    pass
+
 def to_roman(n):
     '''convert integer to Roman numeral'''
     if not (0 < n < 4000):
         raise OutOfRangeError('number out of range (must be 1..3999)')
-        
+    if not isinstance(n, int):
+        raise NotIntegerError('non-integers can not be converted')
+
     result = ''
     for numeral, integer in roman_numeral_map:
         while n >= integer:
